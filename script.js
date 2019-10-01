d6ebd3cbd30275ada1dcf131578c6057c915b091
Kevin Ngo <ngokevin39@gmail.com>
2019-09-30 21:58:23 -0400

    implemented hover over highlighting

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 var lastClicked;
 var count = 0;
 var turn = document.querySelector("span[id='turn']");
-
+var stringTurn = "red";
 var board = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0] ];
+    [0, 0, 0, 0, 0, 0, 0]];
 
-var grid = clickableGrid(6,7,function(el,col){
+var grid = clickableGrid(6, 7, function (el, col) {
     var entireCol = document.querySelectorAll(`td[data-col='${col}']`);
-    for (var r = entireCol.length-1 ; r >= 0; --r){
+    for (var r = entireCol.length - 1; r >= 0; --r) {
         if (entireCol[r].id == "") {
-            if ( count % 2 == 0) {
+            if (count % 2 == 0) {
                 entireCol[r].id = 'red';
                 count++;
                 turn.innerText = "YELLOW";
-            }else {
+                stringTurn = "yellow";
+            } else {
                 entireCol[r].id = 'yellow';
                 count++;
                 turn.innerText = "RED";
+                stringTurn = "red";
             }
             break;
         }
@@ -29,7 +31,7 @@ var grid = clickableGrid(6,7,function(el,col){
 });
 
 document.getElementById("master").appendChild(grid);
-     
+
 
 function convertGridToBoard() {
     for (var row = 0; row < 6; ++row) {
@@ -44,34 +46,34 @@ function convertGridToBoard() {
 }
 
 
-function chkLine(a,b,c,d) {
+function chkLine(a, b, c, d) {
     // Check first cell non-zero and all cells match
-    return ((a != 0) && (a ==b) && (a == c) && (a == d));
+    return ((a != 0) && (a == b) && (a == c) && (a == d));
 }
 
 function chkWinner(bd) {
     // Check down
     for (r = 0; r < 3; r++)
         for (c = 0; c < 7; c++)
-            if (chkLine(bd[r][c], bd[r+1][c], bd[r+2][c], bd[r+3][c]))
+            if (chkLine(bd[r][c], bd[r + 1][c], bd[r + 2][c], bd[r + 3][c]))
                 return bd[r][c];
 
     // Check right
     for (r = 0; r < 6; r++)
         for (c = 0; c < 4; c++)
-            if (chkLine(bd[r][c], bd[r][c+1], bd[r][c+2], bd[r][c+3]))
+            if (chkLine(bd[r][c], bd[r][c + 1], bd[r][c + 2], bd[r][c + 3]))
                 return bd[r][c];
 
     // Check down-right
     for (r = 0; r < 3; r++)
         for (c = 0; c < 4; c++)
-            if (chkLine(bd[r][c], bd[r+1][c+1], bd[r+2][c+2], bd[r+3][c+3]))
+            if (chkLine(bd[r][c], bd[r + 1][c + 1], bd[r + 2][c + 2], bd[r + 3][c + 3]))
                 return bd[r][c];
 
     // Check down-left
     for (r = 3; r < 6; r++)
         for (c = 0; c < 4; c++)
-            if (chkLine(bd[r][c], bd[r-1][c+1], bd[r-2][c+2], bd[r-3][c+3]))
+            if (chkLine(bd[r][c], bd[r - 1][c + 1], bd[r - 2][c + 2], bd[r - 3][c + 3]))
                 return bd[r][c];
 
     return 0;
@@ -86,26 +88,63 @@ function winCheck() {
     }
 }
 
-function clickableGrid( rows, cols, callback ){
+
+function removeHover() {
+    for (var cell of document.querySelectorAll("td[id*='hover']")) {
+        cell.id = "";
+    }
+}
+
+
+function toggleHover(e, toggle) {
+    var cells = document.querySelectorAll(`td[data-col='${e.target.dataset.col}']`);
+    for (var r = 5; r >= 0; --r) {
+        if (toggle) {
+            if (cells[r].id == "") {
+                cells[r].id = "hover-" + stringTurn;
+                break;
+            }
+        } else {
+            var cells = document.querySelectorAll(`td[data-col='${e.target.dataset.col}']`);
+            for (var r = 5; r >= 0; --r) {
+                if (cells[r].id == `hover-${stringTurn}`) {
+                    cells[r].id = "";
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function clickableGrid(rows, cols, callback) {
     var grid = document.createElement('table');
     grid.className = 'grid';
-    for (var r=0;r<rows;++r){
+    for (var r = 0; r < rows; ++r) {
         var tr = document.createElement('tr');
         tr.setAttribute("data-row", r);
         grid.appendChild(tr);
-        for (var c=0;c<cols;++c){
+        for (var c = 0; c < cols; ++c) {
             var cell = document.createElement('td');
             cell.setAttribute("data-col", c);
-            cell.addEventListener('click',(function(el,c){
-                return function(){
-                    callback(el,c);
+            cell.addEventListener('click', (function (el, c) {
+                return function (el) {
+                    removeHover();
+                    callback(el, c);
                     convertGridToBoard();
                     winCheck();
+                    toggleHover(el, true);
                 }
-            })(cell,c),false);
+            })(cell, c), false);
             tr.appendChild(cell);
+
+            cell.addEventListener('mouseover', function (e) {
+                toggleHover(e, true);
+            });
+
+            cell.addEventListener('mouseout', function (e) {
+                toggleHover(e, false);
+            });
         }
     }
     return grid;
 }
-
