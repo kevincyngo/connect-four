2bbb9abc67e6a0c172c2b26a7cce0e8bc8f77ab0
Kevin Ngo <ngokevin39@gmail.com>
2019-10-01 20:20:41 -0400

    implemented replay functionality

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-var lastClicked;
-var count = 0;
-var turn = document.querySelector("span[id='turn']");
-var stringTurn = "red";
-var board = [
+const playerTurn = {
+    red: "red",
+    yellow: "yellow"
+}
+
+//Default values
+const DEFAULT_COUNT = 0;
+const DEFAULT_STRING_TURN = playerTurn.red;
+const DEFAULT_BOARD = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -10,41 +14,24 @@ var board = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0]];
 
-var grid = clickableGrid(6, 7, function (el, col) {
-    var entireCol = document.querySelectorAll(`td[data-col='${col}']`);
-    for (var r = entireCol.length - 1; r >= 0; --r) {
-        if (entireCol[r].id == "") {
-            if (count % 2 == 0) {
-                entireCol[r].id = 'red';
-                count++;
-                turn.innerText = "YELLOW";
-                stringTurn = "yellow";
-            } else {
-                entireCol[r].id = 'yellow';
-                count++;
-                turn.innerText = "RED";
-                stringTurn = "red";
-            }
-            break;
-        }
-    }
-});
-
-document.getElementById("master").appendChild(grid);
-
+//Initialization
+var lastClicked;
+var count = DEFAULT_COUNT;
+var turn = document.querySelector("span[id='turn']");
+var stringTurn = DEFAULT_STRING_TURN;
+var board = DEFAULT_BOARD;
 
 function convertGridToBoard() {
     for (var row = 0; row < 6; ++row) {
         for (var col = 0; col < 7; ++col) {
-            if (document.querySelector(`tr[data-row='${row}'] td[data-col='${col}']`).id == "red") {
+            if (document.querySelector(`tr[data-row='${row}'] td[data-col='${col}']`).id == playerTurn.red) {
                 board[row][col] = 1;
-            } else if (document.querySelector(`tr[data-row='${row}'] td[data-col='${col}']`).id == "yellow") {
+            } else if (document.querySelector(`tr[data-row='${row}'] td[data-col='${col}']`).id == playerTurn.yellow) {
                 board[row][col] = 2;
             }
         }
     }
 }
-
 
 function chkLine(a, b, c, d) {
     // Check first cell non-zero and all cells match
@@ -82,19 +69,20 @@ function chkWinner(bd) {
 function winCheck() {
     var winner = chkWinner(board);
     if (winner == 1) {
-        alert("red win");
+        alert("RED WINS");
     } else if (winner == 2) {
-        alert("yellow win");
+        alert("YELLOW WINS");
+    }
+    if (winner > 0) {
+        reset();
     }
 }
-
 
 function removeHover() {
     for (var cell of document.querySelectorAll("td[id*='hover']")) {
         cell.id = "";
     }
 }
-
 
 function toggleHover(e, toggle) {
     var cells = document.querySelectorAll(`td[data-col='${e.target.dataset.col}']`);
@@ -148,3 +136,59 @@ function clickableGrid(rows, cols, callback) {
     }
     return grid;
 }
+
+function setDisplay(bool) {
+    if (bool) {
+        document.querySelector("#gamePrompt").setAttribute("hidden", true);
+        document.querySelector("#master").removeAttribute("hidden");
+    }
+    else {
+        document.querySelector("#gamePrompt").removeAttribute("hidden");
+        document.querySelector("#master").setAttribute("hidden", true);
+    }
+}
+
+function isPlaying() {
+    setDisplay(true);
+    var grid = clickableGrid(6, 7, function (el, col) {
+        var entireCol = document.querySelectorAll(`td[data-col='${col}']`);
+        for (var r = entireCol.length - 1; r >= 0; --r) {
+            if (entireCol[r].id == "") {
+                if (count % 2 == 0) {
+                    entireCol[r].id = playerTurn.red;
+                    count++;
+                    turn.setAttribute("data-turn", playerTurn.yellow);
+                    turn.innerText = playerTurn.yellow.toUpperCase();
+                    stringTurn = playerTurn.yellow;
+                } else {
+                    entireCol[r].id = playerTurn.yellow;
+                    count++;
+                    turn.setAttribute("data-turn", playerTurn.red);
+                    turn.innerText = playerTurn.red.toUpperCase();
+                    stringTurn = playerTurn.red;
+                }
+                break;
+            }
+        }
+    });
+    document.getElementById("master").appendChild(grid);
+}
+
+function reset() {
+    var usedCells = document.querySelectorAll("td#yellow");
+    for (cell of usedCells) {
+        cell.id = "";
+    }
+    usedCells = document.querySelectorAll("td#red");
+    for (cell of usedCells) {
+        cell.id = "";
+    }
+    setDisplay(false);
+    board = DEFAULT_BOARD;
+    count = DEFAULT_COUNT;
+    stringTurn = DEFAULT_STRING_TURN;
+
+}
+
+//TODO: convertGridToBoard is being called after every click and it is running in a for loop
+//To fix - pass in row and col to convert (can be done in constant time)
